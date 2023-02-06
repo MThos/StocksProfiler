4bbac76fa372f53086cb09a84edf4cc5fb06cfad
MThos <MThos@users.noreply.github.com>
2023-02-06 04:12:43 -0500

    added /stocklist, /news, /topmarket cap and /dailyhistory

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const cors = require('cors');
+const { type } = require('@testing-library/user-event/dist/type');
 const app = express();
 require('dotenv').config();
 
@@ -26,10 +27,16 @@ db.once('connected', () => {
 try {
   const port = process.env.REACT_APP_NODE_PORT
   app.listen(port, () => console.log(`Server is running -> 127.0.0.1 : ${port}.`));
-  cachedStockList();
 } catch (error) {
   console.log(error);
 }
+
+// STOCK LIST
+app.get('/stocklist', (req) => {
+  const STOCKLIST_CACHE_TIME = 1000 * 60 * 60 * 24;; // 60 minutes
+  const STOCKLIST_ENDPOINT = `https://financialmodelingprep.com/api/v3/available-traded/list?apikey=${API_KEY}`;
+  cachedStockList('stocklist', req, STOCKLIST_ENDPOINT, STOCKLIST_CACHE_TIME);
+})
 
 // PROFILE
 app.get('/profile', (req, res) => {
@@ -89,21 +96,21 @@ app.get('/quarterly', (req, res) => {
 
 // INDEXES
 app.get('/indexes', (req, res) => {
-  const INDEX_CACHE_TIME = 1000 * 60 * 5 // 5 minutes
+  const INDEX_CACHE_TIME = 1000 * 60 * 1 // 1 minutes
   const INDEX_ENDPOINT = `https://financialmodelingprep.com/api/v3/quote/%5EGSPC,%5EDJI,%5EIXIC?apikey=${API_KEY}`;
   cachedTickerData('indexes', req, res, INDEX_ENDPOINT, INDEX_CACHE_TIME);
 });
 
 // GAINERS
 app.get('/gainers', (req, res) => {
-  const GAINER_CACHE_TIME = 1000 * 60 * 5 // 5 minutes
+  const GAINER_CACHE_TIME = 1000 * 60 * 1 // 5 minutes
   const GAINER_ENDPOINT = `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=${API_KEY}`;
   cachedTickerData('gainers', req, res, GAINER_ENDPOINT, GAINER_CACHE_TIME);
 });
 
 // LOSERS
 app.get('/losers', (req, res) => {
-  const LOSER_CACHE_TIME = 1000 * 60 * 5 // 5 minutes
+  const LOSER_CACHE_TIME = 1000 * 60 * 1 // 1 minutes
   const LOSER_ENDPOINT = `https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=${API_KEY}`;
   cachedTickerData('losers', req, res, LOSER_ENDPOINT, LOSER_CACHE_TIME);
 });
@@ -122,6 +129,28 @@ app.get('/pricetargetconsensus', (req, res) => {
   const PRICE_TARGET_CONSENSUS_CACHE_TIME = 1000 * 60 * 120 // 120 minutes
   const PRICE_TARGET_CONSENSUS_ENDPOINT = `https://financialmodelingprep.com/api/v4/price-target-consensus?symbol=${SYMBOL}&apikey=${API_KEY}`;
   cachedStockData('pricetargetconsensus', req, res, SYMBOL, PRICE_TARGET_CONSENSUS_ENDPOINT, PRICE_TARGET_CONSENSUS_CACHE_TIME);
+});
+
+// DAILY HISTORICAL PRICING
+app.get('/dailyhistory', (req, res) => {
+  const SYMBOL = req.query.symbol;
+  const DAILY_HISTORY_CACHE_TIME = 1000 * 60 * 5 // 5 minutes
+  const DAILY_HISTORY_ENDPOINT = `https://financialmodelingprep.com/api/v3/historical-price-full/${SYMBOL}?apikey=${API_KEY}`;
+  cachedStockData('dailyhistory', req, res, SYMBOL, DAILY_HISTORY_ENDPOINT, DAILY_HISTORY_CACHE_TIME);
+});
+
+// TOP 100 MARKET CAP
+app.get('/topmarketcap', (req, res) => {
+  const TOP_MARKET_CAP_CACHE_TIME = 1000 * 60 * 1 // 1 minute
+  const TOP_MARKET_CAP_ENDPOINT = `https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&volumeMoreThan=1&exchange=NYSE,NASDAQ,AMEX,EURONEXT,TSX&isActivelyTrading=true&isEtf=false&limit=100&apikey=${API_KEY}`;
+  cachedTickerData('topmarketcap', req, res, TOP_MARKET_CAP_ENDPOINT, TOP_MARKET_CAP_CACHE_TIME);
+});
+
+// NEWS ARTICLES
+app.get('/news', (req, res) => {
+  const NEWS_CACHE_TIME = 1000 * 60 * 1 // 1 minute
+  const NEWS_ENDPOINT = `https://financialmodelingprep.com/api/v3/stock_news?limit=100&apikey=${API_KEY}`;
+  cachedTickerData('news', req, res, NEWS_ENDPOINT, NEWS_CACHE_TIME);
 });
 
 // requires a symbol to be passed
@@ -158,6 +187,8 @@ function cachedStockData(type, req, res, symbol, end_point, cache_time) {
           );
 
           res.json(response.data);
+          access_log(API_KEY, type, end_point, req.socket.remoteAddress, true);
+          call_history();
           console.log(`API request (${type}) -> ${new Date().toUTCString()} -> Client: ${req.socket.remoteAddress}`);
         }).catch((error) => {
           console.log(error); 
@@ -216,6 +247,8 @@ function cachedTickerData(type, req, res, end_point, cache_time) {
           );
 
           res.json(response.data);
+          access_log(API_KEY, type, end_point, req.socket.remoteAddress, true);
+          call_history();
           console.log(`API request (${type}) -> ${new Date().toUTCString()} -> Client: ${req.socket.remoteAddress}`);
         }).catch((error) => {
           console.log(error); 
@@ -241,11 +274,8 @@ function cachedTickerData(type, req, res, end_point, cache_time) {
 }
 
 // does not require a symbol to be passed
-function cachedStockList() {
+function cachedStockList(type, req, end_point, cache_time) {
   try {
-    let type = 'stocklist';
-    let end_point = `https://financialmodelingprep.com/api/v3/available-traded/list?apikey=${API_KEY}`;
-    let cache_time = 1000 * 60 * 60 * 24; // 24 hours
     db.collection(type).countDocuments(
       {
         type: type, 
@@ -276,14 +306,54 @@ function cachedStockList() {
             }
           );
 
-          console.log(`API request (${type}) -> ${new Date().toUTCString()}`);
+          access_log(API_KEY, type, end_point, req.socket.remoteAddress, true);
+          call_history();
+          console.log(`API request (${type}) -> ${new Date().toUTCString()} -> Client: ${req.socket.remoteAddress}`);
         }).catch((error) => {
           console.log(error); 
         });
       } else {
-        console.log(`Database request (${type}) -> ${new Date().toUTCString()}`);
+        console.log(`Database request (${type}) -> ${new Date().toUTCString()} -> Client: ${req.socket.remoteAddress}`);
       }
     });    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// count number of calls in the last minute
+function call_history() {
+  try {
+    db.collection('access_log').countDocuments(
+      {
+        'success': true,
+        'timestamp': {
+          $gt: Date.now() - (1000 * 60) // 1m
+        }
+      }
+    ).then((counter) => {      
+      console.log(`API calls in the last 60s: ${counter}`);
+      return counter;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// log all api calls
+function access_log(api_key, type, end_point, client, success) {
+  try {
+    db.collection('access_log').insertOne(
+      {
+        api_key: api_key, 
+        type: type,
+        end_point: end_point,
+        datetimestamp: new Date().toUTCString(),
+        timestamp: Date.now(),
+        client: client,
+        success: success
+      }
+    );
   } catch (error) {
     console.log(error);
   }
