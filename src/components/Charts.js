6d79d5b6ab0ea49d313ffa9904dc15b41c87fac9
MThos <MThos@users.noreply.github.com>
2023-02-06 04:17:23 -0500

    Finished first version of chart - now selecting 1y chart but will give option for multiple periods soon

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, CartesianAxis } from 'recharts';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, ReferenceLine, Label } from 'recharts';
+import LoadingIcon from './LoadingIcon.js';
+import NoData from './NoData'
 
 const Charts = () => {
-  let data = [
-    {
-      date: '2022/03/16',
-      open: 131,
-      close: 134,
-      high: 137,
-      low: 131,
-    },
-    {
-      date: '2022/03/17',
-      open: 134,
-      close: 133,
-      high: 134,
-      low: 127,
-    },
-    {
-      date: '2022/03/18',
-      open: 133,
-      close: 141,
-      high: 142,
-      low: 133,
-    },
-    {
-      date: '2022/03/19',
-      open: 141,
-      close: 139,
-      high: 144,
-      low: 137,
-    },
-    {
-      date: '2022/03/20',
-      open: 139,
-      close: 141,
-      high: 141,
-      low: 133,
-    }
-  ];
+  const [historicalData, setHistoricalData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const active = (localStorage.getItem('active')) ? localStorage.getItem('active') : 'AAPL';
+  let data = [];
+  
+  if (historicalData && historicalData.length > 0) {
+    data = 
+      historicalData.map((history) => (
+        createData(
+          history.date, 
+          history.open.toFixed(2), 
+          history.high.toFixed(2), 
+          history.low.toFixed(2), 
+          history.close.toFixed(2), 
+          history.volume, 
+          history.changePercent.toFixed(2) + '%')
+      ));
+  }
+
+  const data_1m = data.slice(0, 20).reverse();
+  const data_3m = data.slice(0, 60).reverse();
+  const data_6m = data.slice(0, 120).reverse();
+  const data_1y = data.slice(0, 250).reverse();
+  const data_5y = data.slice(0, 1250).reverse();
+  const data_mx = data.reverse();
+  const current_close = data_1y.map((row) => row.close);
+
+  function createData(date, open, high, low, close, volume, change) {
+    return { date, open, high, low, close, volume, change };
+  };
 
   const tooltipContentStyle = {
     border: "2px solid black",
-    padding: "15px"
+    fontWeight: "600",
+    padding: "15px",
+    textAlign: "center",
+    textTransform: "uppercase"
   };
 
   const tooltipLabelStyle = {
     fontWeight: "600",
+    fontSize: "1.2rem",
     paddingBottom: "15px",
     textAlign: "center"
   };
@@ -56,23 +56,62 @@ const Charts = () => {
     paddingLeft: "20px"
   }
 
-  return(
-    <section className="fade-in">
-      <ResponsiveContainer width={"80%"} height={300} id="daily-chart">
-        <BarChart data={data}>
-          <CartesianAxis />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip contentStyle={tooltipContentStyle} labelStyle={tooltipLabelStyle} />
-          <Legend wrapperStyle={legendWrapperStyle} />
-          <Bar type="linear" dataKey="open" stroke="black" fill="black" />
-          <Bar type="linear" dataKey="close" stroke="black" fill="black" />
-          <Bar type="linear" dataKey="high" stroke="black" fill="mediumseagreen" />
-          <Bar type="linear" dataKey="low" stroke="black" fill="red" />
-        </BarChart>
+  useEffect(() => {
+    try {
+      setLoading(true);
+      
+      const options = {
+        method: 'GET',
+        url: 'http://localhost:8000/dailyhistory',
+        params: { symbol: active }
+      };
+  
+      axios.request(options).then((response) => {
+        //console.log(response.data.historical);
+        setHistoricalData(response.data.historical);
+        setLoading(false);
+      }).catch((error) => {
+        console.log(error);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <LoadingIcon />
+    )
+  }
+
+  if (historicalData && Object.keys(historicalData).length > 0) {
+  return (
+    <section id="chart" className="fade-in">
+      <div id="chart-symbol">
+        {active}
+      </div>
+      <ResponsiveContainer width={"80%"} height={500} id="daily-chart">
+        <AreaChart data={data_1y}>
+          <XAxis dataKey="date" dy={10} />
+          <YAxis dataKey="close" dx={-5} domain={['dataMin', 'dataMax']} />
+          <Tooltip offset={100} separator={': '} contentStyle={tooltipContentStyle} labelStyle={tooltipLabelStyle} />
+          <Legend wrapperStyle={legendWrapperStyle} iconSize={20} />
+          <ReferenceLine y={current_close.at(-1)} stroke="black" strokeWidth={2} label="">
+            <Label value={`$${current_close.at(-1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`} position="insideTop" offset={10} stroke="black" strokeWidth={1} fontWeight={400} fontSize={'1.1rem'} />
+          </ReferenceLine>
+          <Area type="monotone" dataKey="close" stroke="black" strokeWidth={1} fillOpacity={0.6} fill="mediumslateblue" />
+          <Area type="monotone" dataKey="open" stroke="black" strokeWidth={0} fillOpacity={0} fill="black" />
+          <Area type="monotone" dataKey="low" stroke="red" strokeWidth={0} fillOpacity={0} fill="black" />
+          <Area type="monotone" dataKey="high" stroke="mediumseagreen" strokeWidth={0} fillOpacity={0} fill="black" />
+          <Area type="monotone" dataKey="change" stroke="black" strokeWidth={0} fillOpacity={0} fill="black" />
+        </AreaChart>
       </ResponsiveContainer>
-    </section>  
-  )
+    </section>)
+  } else {
+    return (
+      <NoData page="charts" />
+    );
+  }
 }
 
 export default Charts;
