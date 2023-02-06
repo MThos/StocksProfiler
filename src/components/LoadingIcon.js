dbdcd2c459edf0c6c9e4635773e84b323ac2e07a
MThos <MThos@users.noreply.github.com>
2023-02-06 04:13:23 -0500

    Create LoadingIcon.js

---
+++ src/components/LoadingIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoopIcon from '@mui/icons-material/Loop';
+
+const LoadingIcon = () => {
+  return (
+    <section id="loading">
+      <LoopIcon id="loading-icon" />
+      <span id="loading-text">LOADING DATA</span>
+    </section>
+  )
+}
+
+export default LoadingIcon;
